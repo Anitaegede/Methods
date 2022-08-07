5a6c7ed4d9863b91d402773b4f6b5598b96b0cd7
Anitaegede <debbyat2008@gmail.com>
2022-08-07 09:36:05 +0100

    fourth

---
+++ string-methods.js
@@ -0,0 +1,13 @@
+let string = "Slow and steady wins the race";
+console.log(string.slice(9));
+
+let sent = "The sunset is beautiful";
+console.log(sent.toUpperCase());
+
+let words = "The sunset is beautiful";
+console.log(sent.toLowerCase());
+
+let greeting =   "   Good Morning    ";
+console.log(greeting);
+console.log(greeting.trim());
+
