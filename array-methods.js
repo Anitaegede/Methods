492ef9a22ef610007b5327d31facde06e863a3be
Anitaegede <debbyat2008@gmail.com>
2022-08-07 09:12:08 +0100

    second

---
+++ array-methods.js
@@ -0,0 +1,11 @@
+let first =["clothes","shoes"]
+let second = ["bags","jewellry"]
+let both = first.concat(second)
+console.log(both)
+ 
+let cars = ["BMW","Lexus","Mercedes Benz","Toyota","Ford"]
+let all = cars.constructor
+console.log(all)
+let days = ["Sunday","Wednesday","Monday"]
+days.sort()
+console.log(days)
